82827fa22c9810bbab8817ab01823ef623d21b45
as-ariahi <akram.riahi.extern@aviv-group.com>
2022-01-25 00:03:16 +0100

    update app

--- app/src/index.php
+++ app/src/index.php
@@ -1,5 +1,6 @@
 <?php
 
 echo "Hello, from ChaosCarnival 2022! <br>";
+echo "Dont forget to put a good bg";
 echo '<img src="https://github.com/litmuschaos/litmus/blob/master/images/litmus-logo-dark-bg-sidebyside.png">';
 ?>
