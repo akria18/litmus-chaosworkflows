9a669b74a352aa7c9f0a809c9ffbd56228b60025
as-ariahi <akram.riahi.extern@aviv-group.com>
2022-01-27 17:58:26 +0100

    trigger chaos

--- app/src/index.php
+++ app/src/index.php
@@ -1,6 +1,6 @@
 <center><?php
 
-echo '<span style="font-size: medium; font-weight: normal;"> Hello, from ChaosCarnival 2022! </span> <br>';
+echo '<span style="font-size: medium; font-weight: normal;"> Hello chaos Folk, from ChaosCarnival 2022! </span> <br>';
 echo '<img src="https://www.chaosnative.com/_next/image?url=%2Fhome%2Fclc.png&w=1920&q=75">';
 
 ?></center>
