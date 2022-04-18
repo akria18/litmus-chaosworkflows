674d5407c67c4d56be3d2d794a00db16982bbc77
as-ariahi <akram.riahi.extern@aviv-group.com>
2022-04-18 09:52:35 +0200

    unleash the chaos

--- app/src/index.php
+++ app/src/index.php
@@ -1,6 +1,6 @@
 <center><?php
 
-echo '<span style="font-size: medium; font-weight: normal;"> Hello Chaos Folks from arround the world, from Devoxx  2022! </span> <br>';
+echo '<span style="font-size: medium; font-weight: normal;"> Hello Chaos Folks from arround the world, from Devoxx  2022!, Happy to see you ! </span> <br>';
 echo '<img src="https://www.chaosnative.com/_next/image?url=%2Fhome%2Fclc.png&w=1920&q=75">';
 
 ?></center>
